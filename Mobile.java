6fd5adc429d1c93fb69bb401712dc2d41a9bafd3
javierdarko <javier.darko42@gmail.com>
2018-05-14 00:54:19 -0500

    Listo

---
+++ Mobile.java
@@ -0,0 +1,10 @@
+package com.java9s.designpattern.factory;
+
+public interface Mobile {
+	public static final String IPHONE="IPHONE";
+	public static final String SONY="sony";
+	public static final String SAMSUNG="samsung";
+        public static final String MOTOROLA="motorola";
+        public static final String XIAOMI="xiamoi";
+        public static final String HUAWEI="huawei";
+}
---
+++ MobileFactory.java
@@ -0,0 +1,21 @@
+package com.java9s.designpattern.factory;
+
+public class MobileFactory {
+	public static Mobile createMobile(String type){
+		if(type.equals(Mobile.IPHONE)){
+			return new IPhone(2, "A9", "A9 GPu");
+		}else if(type.equals(Mobile.SONY)){
+			return new Sony(2,"ARM");
+		}else if(type.equals(Mobile.SAMSUNG)){
+			return new Samsung("Intel");
+		}else if(type.equals(Mobile.MOTOROLA)){
+			return new Motorola("Adreno 506");
+                }else if(type.equals(Mobile.XIAMOI)){
+			return new Xaomi("Qualcomm SnapDragon");
+                }else if(type.equals(Mobile.HUAWEI)){
+			return new Huawei("Mali-T830");
+                }else {
+			return null;
+		}
+	}
+}
